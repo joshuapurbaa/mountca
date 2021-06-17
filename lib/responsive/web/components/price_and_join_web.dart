7e236ccf70ed52e0eb79fe4d9452c10b3caba898
thejo06 <yoshuapurba06@gmail.com>
2021-06-17 18:00:49 +0700

    "fix errors"

--- lib/responsive/web/components/price_and_join_web.dart
+++ lib/responsive/web/components/price_and_join_web.dart
@@ -10,48 +10,46 @@ class PriceAndJoinWeb extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Expanded(
-      child: Container(
-        padding: EdgeInsets.symmetric(horizontal: 24, vertical: 16),
-        child: Row(
-          children: [
-            Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  'Rp. 19.000',
-                  style: boldText17.copyWith(color: neonBlueColor),
-                ),
-                Text(
-                  '/orang',
-                  style: mediumText.copyWith(fontSize: 12),
-                )
-              ],
-            ),
-            Spacer(),
-            InkWell(
-              onTap: () {
-                Navigator.pushNamed(context, TeamMemberScreen.nameRoute);
-              },
-              child: Container(
-                height: 68,
-                width: MediaQuery.of(context).size.width * 0.12,
-                alignment: Alignment.center,
-                decoration: BoxDecoration(
-                  color: neonBlueColor,
-                  borderRadius: BorderRadius.all(
-                    Radius.circular(10),
-                  ),
-                ),
-                child: Text(
-                  'Gabung tim',
-                  style: boldText14.copyWith(color: whiteColor),
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 24, vertical: 16),
+      child: Row(
+        children: [
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Rp. 19.000',
+                style: boldText17.copyWith(color: neonBlueColor),
+              ),
+              Text(
+                '/orang',
+                style: mediumText.copyWith(fontSize: 12),
+              )
+            ],
+          ),
+          Spacer(),
+          InkWell(
+            onTap: () {
+              Navigator.pushNamed(context, TeamMemberScreen.nameRoute);
+            },
+            child: Container(
+              height: 68,
+              width: MediaQuery.of(context).size.width * 0.12,
+              alignment: Alignment.center,
+              decoration: BoxDecoration(
+                color: neonBlueColor,
+                borderRadius: BorderRadius.all(
+                  Radius.circular(10),
                 ),
               ),
+              child: Text(
+                'Gabung tim',
+                style: boldText14.copyWith(color: whiteColor),
+              ),
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -21,65 +21,62 @@ class HomePage extends StatelessWidget {
           ),
           Container(
             height: mediaQueryHeight,
-            child: Expanded(
-              flex: 1,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: [
-                  SizedBox(
-                    height: mediaQueryHeight * 0.12,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                SizedBox(
+                  height: mediaQueryHeight * 0.12,
+                ),
+                Image.asset(
+                  'assets/images/logo-homepage.png',
+                  height: 100,
+                ),
+                Text(
+                  'Mountca',
+                  style: boldText50.copyWith(
+                    color: whiteColor,
                   ),
-                  Image.asset(
-                    'assets/images/logo-homepage.png',
-                    height: 100,
+                  textAlign: TextAlign.center,
+                ),
+                Spacer(),
+                Text(
+                  'Grouping & Hiking',
+                  style: semiBoldText25.copyWith(
+                    color: greyNobelColor,
                   ),
-                  Text(
-                    'Mountca',
-                    style: boldText50.copyWith(
-                      color: whiteColor,
+                  textAlign: TextAlign.center,
+                ),
+                SizedBox(
+                  height: 5,
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 24),
+                  child: Text(
+                    'Find a group for hiking, and get the best\ninformation for your destination',
+                    style: semiBoldText12.copyWith(
+                      color: greyNobelColor.withOpacity(0.7),
                     ),
                     textAlign: TextAlign.center,
                   ),
-                  Spacer(),
-                  Text(
-                    'Grouping & Hiking',
-                    style: semiBoldText25.copyWith(
-                      color: greyNobelColor,
-                    ),
-                    textAlign: TextAlign.center,
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                TextButton(
+                  onPressed: () {
+                    Navigator.pushReplacementNamed(
+                        context, MountainDestination.nameRoute);
+                  },
+                  child: Text(
+                    'Get Started',
+                    style: boldText16,
                   ),
-                  SizedBox(
-                    height: 5,
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 24),
-                    child: Text(
-                      'Find a group for hiking, and get the best\ninformation for your destination',
-                      style: semiBoldText12.copyWith(
-                        color: greyNobelColor.withOpacity(0.7),
-                      ),
-                      textAlign: TextAlign.center,
-                    ),
-                  ),
-                  SizedBox(
-                    height: 20,
-                  ),
-                  TextButton(
-                    onPressed: () {
-                      Navigator.pushReplacementNamed(
-                          context, MountainDestination.nameRoute);
-                    },
-                    child: Text(
-                      'Get Started',
-                      style: boldText16,
-                    ),
-                  ),
-                  SizedBox(
-                    height: mediaQueryHeight * 0.10,
-                  ),
-                ],
-              ),
+                ),
+                SizedBox(
+                  height: mediaQueryHeight * 0.10,
+                ),
+              ],
             ),
           )
         ],
